e63a876c85acf29ce646aa1085bf39b7b8188e9e
graemeerickson <graemeerickson@gmail.com>
2019-01-28 09:39:53 -0800

    Added public path and set historyApiFallback devServer property to true in order to enable successful refresh within the app

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './app/index.js',
   output: {
+    filename: 'index_bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    publicPath: '/',
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
     ]
   },
   mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
