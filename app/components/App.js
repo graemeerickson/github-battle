4196d4fc02e9a5da614a71a19c937d46e7065923
graemeerickson <graemeerickson@gmail.com>
2019-01-28 09:22:19 -0800

    Implemented React Router. Added Nav component.

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,9 @@
 const React = require('react');
 const Popular = require('./Popular');
+const ReactRouter = require('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Nav = require('./Nav');
 /*
   Component parts:
     (1) state (optional)
@@ -14,9 +18,12 @@ class App extends React.Component {
         React.createElement("div", null, "Hello World!"")
     */
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
     )
   }
 }
