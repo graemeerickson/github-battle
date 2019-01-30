3a8b523d964addbf1ae9456132b151381aa2a79c
graemeerickson <graemeerickson@gmail.com>
2019-01-30 09:32:12 -0800

    Implemented battle results functionality, which depends heavily on calling the battle() function to get user details from GitHub, calculate scores, and return the results to the Results component

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,9 +1,79 @@
 const React = require('react');
+const queryString = require('query-string');
+const api = require('../utils/api');
+const Link = require('react-router-dom').Link;
+const Player = require('./Player');
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true
+    }
+  }
+
+  componentDidMount() {
+    const players = queryString.parse(this.props.location.search);
+
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function(players) {
+      if (players === null) {
+        return this.setState(function() {
+          return {
+            error: 'Looks like there was an error. Check that both users exist on GitHub.',
+            loading: false
+          }
+        });
+      }
+      
+      this.setState(function() {
+        return {
+          error: null,
+          winner: players[0],
+          loser: players[1],
+          loading: false
+        }
+      });
+    }.bind(this));
+  }
+
   render() {
+    const error = this.state.error;
+    const winner = this.state.winner;
+    const loser = this.state.loser;
+    const loading = this.state.loading;
+
+    if (loading === true) {
+      return <p>Loading results...</p>
+    }
+
+    if (error) {
+      return (
+        <div>
+          <p>{error}</p>
+          <Link to='/battle'>Reset</Link>
+        </div>
+      )
+    }
+
     return (
-      <div>Results</div>
+      <div className='row'>
+        <Player
+          label='Winner'
+          score={winner.score}
+          profile={winner.profile}
+        />
+        <Player
+          label='Loser'
+          score={loser.score}
+          profile={loser.profile}
+        />
+      </div>
     )
   }
 }
