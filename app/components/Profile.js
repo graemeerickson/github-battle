da2d5cd60324ff867045f6658fc894ca653d0974
graemeerickson <graemeerickson@gmail.com>
2019-01-31 11:25:45 -0800

    ES6 revamp: const, arrow functions, object destructuring

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -2,21 +2,21 @@ const React = require('react');
 const PropTypes = require('prop-types');
 const PlayerPreview = require('./PlayerPreview');
 
-function Profile(props) {
-  const info = props.info;
+const Profile = ({ info }) => {
+  const { avatar_url, blog, company, followers, following, location, login, name, public_repos } = info;
   return (
     <PlayerPreview
-      avatar={info.avatar_url}
-      username={info.login}
+      avatar={avatar_url}
+      username={login}
     >
       <ul className='space-list-items'>
-        {info.name && <li>{info.name}</li>}
-        {info.location && <li>{info.location}</li>}
-        {info.company && <li>{info.company}</li>}
-        <li>Followers: {info.followers}</li>
-        <li>Following: {info.following}</li>
-        <li>Public Repos: {info.public_repos}</li>
-        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+        {name && <li>{name}</li>}
+        {location && <li>{location}</li>}
+        {company && <li>{company}</li>}
+        <li>Followers: {followers}</li>
+        <li>Following: {following}</li>
+        <li>Public Repos: {public_repos}</li>
+        {blog && <li><a href={blog}>{blog}</a></li>}
       </ul>
     </PlayerPreview>
   )
